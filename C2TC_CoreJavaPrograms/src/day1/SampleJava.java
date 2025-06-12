6662bd0def513f0a91e03b920d3231907df0bac5
Martin david <martindavid790430@gmail.com>
2025-06-12 12:23:59 +0530

    addition

--- C2TC_CoreJavaPrograms/src/day1/SampleJava.java
+++ C2TC_CoreJavaPrograms/src/day1/SampleJava.java
@@ -1,10 +1,9 @@
 package day1;
 
-public class SampleJava {
+public class SampleJava {// executed class
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//static helps to run main method automatically whenever the call name called in JVM
 		System.out.println("welcome");
-
 	}
 
 }
